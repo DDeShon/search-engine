bee761b85d2085f34fbb8c4d2011a0ab40d6b3c4
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-07-29 08:20:43 -0500

    added searchBar.js file

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 document.addEventListener("readystatechange", (event) => {
-    if (event.target.readyState === "complete") {
-        initApp();
-    }
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
 });
 
 const initApp = () => {
-    // set the focus on the text input
+  // set the focus on the text input
 
-    // 3 listeners for clear text button
+  // 3 listeners for clear text button
 
-    const form = document.getElementById("searchBar");
-    form.addEventListener("submit" submitTheSearch);
-
-}
+  const form = document.getElementById("searchBar");
+  form.addEventListener("submit", submitTheSearch);
+};
 
 // Procedural "workflow" function
 const submitTheSearch = (event) => {
-    event.preventDefault();
-    // delete the search results
-    // process the search
-    // set the focus
-}
+  event.preventDefault();
+  // delete the search results
+  // process the search
+  // set the focus
+};
