c3b2bf5a1ecc11019c1a156fd638a230e4a559f7
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-07-29 08:26:22 -0500

    added dataFunctions.js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { setSearchFocus } from "./searchBar.js";
+
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "complete") {
     initApp();
@@ -6,6 +8,7 @@ document.addEventListener("readystatechange", (event) => {
 
 const initApp = () => {
   // set the focus on the text input
+  setSearchFocus();
 
   // 3 listeners for clear text button
 
@@ -18,5 +21,11 @@ const submitTheSearch = (event) => {
   event.preventDefault();
   // delete the search results
   // process the search
+  processTheSearch();
   // set the focus
+};
+
+const processTheSearch = async () => {
+  // clear the stats line
+  const searchTerm = getSearchTerm();
 };
