38ca6a5bc7eb6d1e4fe7e80c5a733ee51ac3affb
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-07-29 08:18:57 -0500

    adding javascript

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+});
+
+const initApp = () => {
+    // set the focus on the text input
+
+    // 3 listeners for clear text button
+
+    const form = document.getElementById("searchBar");
+    form.addEventListener("submit" submitTheSearch);
+
+}
+
+// Procedural "workflow" function
+const submitTheSearch = (event) => {
+    event.preventDefault();
+    // delete the search results
+    // process the search
+    // set the focus
+}
